f278b64c2fb95c01f76e3df42dd5b4cfe3eb6456
mkociszewski <mkociszewski@consdata.pl>
2020-11-13 20:14:49 +0100

    Beginning

--- src/main/java/com/consdata/tech/HelpdeskApplication.java
+++ src/main/java/com/consdata/tech/HelpdeskApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SomeApplication {
+public class HelpdeskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SomeApplication.class, args);
+        SpringApplication.run(HelpdeskApplication.class, args);
     }
 }
 
