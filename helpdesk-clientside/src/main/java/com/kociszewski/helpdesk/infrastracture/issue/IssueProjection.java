acc81be94eebf48f6efe1eeb29c071987ea19cd9
mkociszewski <mkociszewski@consdata.pl>
2020-12-05 21:36:12 +0100

    Starting point

--- helpdesk-clientside/src/main/java/com/kociszewski/helpdesk/infrastracture/issue/IssueProjection.java
+++ helpdesk-clientside/src/main/java/com/kociszewski/helpdesk/infrastracture/issue/IssueProjection.java
@@ -1,49 +1,4 @@
 package com.kociszewski.helpdesk.infrastracture.issue;
 
-import com.kociszewski.helpdesk.domain.client.GetClientIssuesQuery;
-import com.kociszewski.helpdesk.domain.issue.Issue;
-import com.kociszewski.helpdesk.domain.issue.IssueClosedEvent;
-import com.kociszewski.helpdesk.domain.issue.IssueCreatedEvent;
-import com.kociszewski.helpdesk.domain.issue.IssueResolvedEvent;
-import org.axonframework.eventhandling.EventHandler;
-import org.axonframework.queryhandling.QueryHandler;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
 public class IssueProjection {
-
-    private final IssueRepository issueRepository;
-
-    public IssueProjection(IssueRepository issueRepository) {
-        this.issueRepository = issueRepository;
-    }
-
-    @EventHandler
-    public void handle(IssueCreatedEvent event) {
-        IssueDTO issue = IssueDTO.builder()
-                .issueId(event.getIssueId())
-                .title(event.getTitle())
-                .description(event.getDescription())
-                .status(Issue.IssueStatus.OPEN.name())
-                .clientId(event.getClientId())
-                .build();
-        issueRepository.insert(issue);
-    }
-
-    @QueryHandler
-    public List<IssueDTO> handle(GetClientIssuesQuery query) {
-        return issueRepository.findAllByClientId(query.getClientId());
-    }
-
-    @EventHandler
-    public void handle(IssueClosedEvent event) {
-        issueRepository.updateStatusByIssueId(event.getIssueId(), Issue.IssueStatus.CLOSED.name());
-    }
-
-    @EventHandler
-    public void handle(IssueResolvedEvent event) {
-        issueRepository.updateStatusByIssueId(event.getIssueId(), Issue.IssueStatus.RESOLVED.name());
-    }
 }
