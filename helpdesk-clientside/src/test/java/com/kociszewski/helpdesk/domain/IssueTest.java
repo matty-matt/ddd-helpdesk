2bd38e525b45dc8b28d177dfa6d0ee28fcd697bf
mkociszewski <mkociszewski@consdata.pl>
2020-12-05 21:58:38 +0100

    Tests done

--- helpdesk-clientside/src/test/java/com/kociszewski/helpdesk/domain/IssueTest.java
+++ helpdesk-clientside/src/test/java/com/kociszewski/helpdesk/domain/IssueTest.java
@@ -5,8 +5,11 @@ import com.kociszewski.helpdesk.domain.issue.*;
 import org.axonframework.test.aggregate.AggregateTestFixture;
 import org.axonframework.test.aggregate.FixtureConfiguration;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class IssueTest {
 
@@ -16,6 +19,7 @@ public class IssueTest {
     private String title;
     private String description;
 
+
     @BeforeEach
     public void setup() {
         this.fixture = new AggregateTestFixture<>(Issue.class);
@@ -24,5 +28,57 @@ public class IssueTest {
         this.title = "Issue title!";
         this.description = "Short description";
     }
-}
 
+    @Test
+    public void shouldCreateIssue() {
+        this.fixture
+                .givenNoPriorActivity()
+                .when(new CreateIssueCommand(issueId, clientId, title, description))
+                .expectEvents(new IssueCreatedEvent(issueId, clientId, title, description))
+                .expectState(state -> {
+                    assertThat(state.getIssueId()).isEqualTo(issueId);
+                    assertThat(state.getClientId()).isEqualTo(clientId);
+                    assertThat(state.getDescription()).isEqualTo(description);
+                    assertThat(state.getTitle()).isEqualTo(title);
+                    assertThat(state.getStatus()).isEqualTo(Issue.IssueStatus.OPEN);
+                });
+    }
+
+    @Test
+    public void shouldResolveIssue() {
+        this.fixture
+                .given(new IssueCreatedEvent(issueId, clientId, title, description))
+                .when(new ResolveIssueCommand(issueId))
+                .expectEvents(new IssueResolvedEvent(issueId))
+                .expectState(state -> {
+                    assertThat(state.getStatus()).isEqualTo(Issue.IssueStatus.RESOLVED);
+                });
+    }
+
+    @Test
+    public void shouldCloseIssue() {
+        this.fixture
+                .given(
+                        new IssueCreatedEvent(issueId, clientId, title, description),
+                        new IssueResolvedEvent(issueId))
+                .when(new CloseIssueCommand(issueId))
+                .expectEvents(new IssueClosedEvent(issueId))
+                .expectState(state -> {
+                    assertThat(state.getStatus()).isEqualTo(Issue.IssueStatus.CLOSED);
+                });
+    }
+
+    @Test
+    public void shouldReopenIssue() {
+        this.fixture
+                .given(
+                        new IssueCreatedEvent(issueId, clientId, title, description),
+                        new IssueResolvedEvent(issueId),
+                        new IssueClosedEvent(issueId))
+                .when(new ReopenIssueCommand(issueId))
+                .expectEvents(new IssueReopenedEvent(issueId))
+                .expectState(state -> {
+                    assertThat(state.getStatus()).isEqualTo(Issue.IssueStatus.OPEN);
+                });
+    }
+}
