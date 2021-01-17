909fe3a385d96922984d733fee5f758c1a2739a5
mkociszewski <mkociszewski@consdata.pl>
2021-01-17 19:59:09 +0100

    add another request

--- helpdesk-clientside/src/main/java/com/kociszewski/helpdesk/infrastracture/issue/IssueProjection.java
+++ helpdesk-clientside/src/main/java/com/kociszewski/helpdesk/infrastracture/issue/IssueProjection.java
@@ -1,4 +1,13 @@
 package com.kociszewski.helpdesk.infrastracture.issue;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class IssueProjection {
+
+    private IssueRepository issueRepository;
+
+    public IssueProjection(IssueRepository issueRepository) {
+        this.issueRepository = issueRepository;
+    }
 }
